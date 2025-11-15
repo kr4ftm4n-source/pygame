6a3a902c1cfb1cdcaed177a9d8acc186b34d73c0
kr4ftman <kr4ftm4n@gmail.com>
2025-11-15 13:07:43 +0100

    instance first window with pygame

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame 
+
+pygame.init()
+
+pygame.display.set_mode((800,600))
+pygame.display.set_caption("Pygamon -- Aventure")
+
+running = True 
+
+while running : 
+    for event in pygame.event.get() : 
+        if event.type == pygame.QUIT:
+            running = False 
+
+pygame.quit()
